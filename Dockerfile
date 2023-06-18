e186665807134dbeb47e2ed839077edeec67a825
JunyaoC <junyaoxiandingchan@gmail.com>
2023-06-18 17:07:06 +0800

    change order

--- Dockerfile
+++ Dockerfile
@@ -23,12 +23,12 @@ RUN pip install -r requirements.txt
 
 RUN CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python
 
-RUN wget https://huggingface.co/TheBloke/stable-vicuna-13B-GGML/resolve/main/stable-vicuna-13B.ggmlv3.q5_1.bin
+# RUN wget https://huggingface.co/TheBloke/stable-vicuna-13B-GGML/resolve/main/stable-vicuna-13B.ggmlv3.q5_1.bin
 
 # # Add your model weight files 
 # # (in this case we have a python script)
-# ADD download.py .
-# RUN python3 download.py
+ADD download.py .
+RUN python3 download.py
 
 ADD . .
 
--- download.py
+++ download.py
@@ -1,11 +1,27 @@
-# This file runs during container build time to get model weights built into the container
+# # This file runs during container build time to get model weights built into the container
 
-# In this example: A Huggingface BERT model
-from transformers import pipeline
+# # In this example: A Huggingface BERT model
+# from transformers import pipeline
 
-def download_model():
-    # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+# def download_model():
+#     # do a dry run of loading the huggingface model, which will download weights
+#     # pipeline('fill-mask', model='bert-base-uncased')
+    
 
-if __name__ == "__main__":
-    download_model()
+# if __name__ == "__main__":
+#     download_model()
+
+import os
+import wget
+
+# URL of the file to download
+url = "https://huggingface.co/TheBloke/stable-vicuna-13B-GGML/resolve/main/stable-vicuna-13B.ggmlv3.q5_1.bin"
+
+# Download the file using wget
+downloaded_file = wget.download(url)
+
+# Get the full path of the downloaded file
+downloaded_file_path = os.path.join(os.getcwd(), downloaded_file)
+
+# Print the full path of the downloaded file
+print("Downloaded file path:", downloaded_file_path)
